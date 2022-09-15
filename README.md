fc0b5a2f0c622d7c9e69cbce45fbafea10c22eb3
LinoyHorvat <linoy.horvat@gmail.com>
2022-09-15 17:28:32 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h2>To get the app running locally:</h2>
+
+1. Clone the the repository.
+2. Install the node modules <code>npm install</code>.
+3. Run the <code>app npm start</code>.
+4. The app available at http://localhost:8000.
+5. Access the paths mentioned above by using Postman.
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const path = require("path");
+require("./db/mongoose");
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+const publicPath = path.join(__dirname, "../client/build");
+app.use(express.static(publicPath));
+
+// const phonebookRoute = require("./routes/phonebook")
+// app.use("/api", phonebookRoute);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
