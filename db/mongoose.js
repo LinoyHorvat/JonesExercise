7682fd9cadc60dd131d42d44e0dc9a0cea20805b
LinoyHorvat <linoy.horvat@gmail.com>
2022-09-21 09:27:11 +0300

    final updates

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,8 +2,9 @@
 Import
 /*----------------------------------------------------------------*/
 const mongoose = require('mongoose');
-require('dotenv').config();
 const mongo_uri = process.env.MONGO_URL;
+
+require('dotenv').config();
 
 mongoose.connect(mongo_uri, () => {
     console.log("connected to db");
